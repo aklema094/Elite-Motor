ea2feea04920767c1d37a9a5ab0577bd493db85a
aklema094 <aklemasultana094@gmail.com>
2025-01-07 08:16:29 +0600

    fixed some bugs

--- src/elitemotor/EliteMotor.java
+++ src/elitemotor/EliteMotor.java
@@ -13,12 +13,12 @@ public class EliteMotor {
 
     public static void main(String[] args) throws InterruptedException {
         Scanner sc = new Scanner(System.in);
-        Showroom shr[] = new Showroom[10];
-        Employees empp[] = new Employees[10];
-        Bike bkk[] = new Bike[10];
-        int is = 0;
-        int je = 0;
-        int kb = 0;
+        Showroom showroom[] = new Showroom[5];
+        Employees employee[] = new Employees[5];
+        Bike bike[] = new Bike[5];
+        int bike_counter = 0;
+        int showroom_counter = 0;
+        int employees_counter = 0;
 
         while (true) {
             System.out.println("========== WELCOME TO ELITE MOTORS==========");
@@ -41,53 +41,80 @@ public class EliteMotor {
             while (ch != 9) {
                 switch (ch) {
                     case 1:
-                        shr[is] = new Showroom(sc);
-                        shr[is].setDetails();
-                        is++;
-                        System.out.println("1. Add new Showroom");
-                        System.out.println("9. Go to main menu");
+                        showroom[showroom_counter] = new Showroom(sc);
+                        showroom[showroom_counter].setDetails();
+                        showroom_counter++;
+                        System.out.println();
+                        System.out.println("1].ADD NEW SHOWROOM");
+                        System.out.println("9].GO BACK TO MAIN MENU");
                         ch = sc.nextInt();
-                        System.out.println("");
                         break;
                     case 2:
-                        empp[je] = new Employees(sc);
-                        empp[je].setDetails();
-                        je++;
-                        System.out.println("2. Add new Employee");
-                        System.out.println("9. Go to main menu");
+                        employee[employees_counter] = new Employees(sc);
+                        employee[employees_counter].setDetails();
+                        employees_counter++;
+                        System.out.println();
+                        System.out.println("2].ADD NEW EMPLOYEE");
+                        System.out.println("9].GO BACK TO MAIN MENU");
                         ch = sc.nextInt();
                         break;
                     case 3:
-                        bkk[kb] = new Bike(sc);
-                        bkk[kb].setDetails();
-                        kb++;
-                        System.out.println("3. Add new Bike");
-                        System.out.println("9. Go to main menu");
+                        bike[bike_counter] = new Bike(sc);
+                        bike[bike_counter].setDetails();
+                        bike_counter++;
+                        System.out.println();
+                        System.out.println("3].ADD NEW CAR");
+                        System.out.println("9].GO BACK TO MAIN MENU");
                         ch = sc.nextInt();
                         break;
                     case 4:
-                        for (int i = 0; i < is; i++) {
-                            shr[i].getDetails();
+                        if (showroom_counter == 0) {
+                            System.out.println("No show room Available");
+                        } else {
+                            for (int i = 0; i < showroom_counter; i++) {
+                                showroom[i].getDetails();
+                                System.out.println();
+                                System.out.println();
+                            }
                         }
-                        System.out.println("9. Go to main menu");
-                        System.out.println("0. Exit");
+                        System.out.println();
+                        System.out.println("9].GO BACK TO MAIN MENU");
+                        System.out.println("0].EXIT");
+                        ch = sc.nextInt();
                         break;
                     case 5:
-                        for (int j = 0; j < je; j++) {
-                            empp[j].getDetails();
+                        if (employees_counter == 0) {
+                            System.out.println("No show Employee Available");
+                        } else {
+                            for (int i = 0; i < employees_counter; i++) {
+                                employee[i].getDetails();
+                                System.out.println();
+                                System.out.println();
+                            }
                         }
-                        System.out.println("9. Go to main menu");
-                        System.out.println("0. Exit");
-
+                        System.out.println();
+                        System.out.println("9].GO BACK TO MAIN MENU");
+                        System.out.println("0].EXIT");
+                        ch = sc.nextInt();
                         break;
                     case 6:
-                        for (int i = 0; i < kb; i++) {
-                            bkk[i].getDetails();
+                        if (bike_counter == 0) {
+                            System.out.println("No show Bike Available");
+                        } else {
+
+                            for (int i = 0; i < bike_counter; i++) {
+                                bike[i].getDetails();
+                                System.out.println();
+                                System.out.println();
+                            }
                         }
-                        System.out.println("9. Go to main menu");
-                        System.out.println("0. Exit");
+                        System.out.println();
+                        System.out.println("9].GO BACK TO MAIN MENU");
+                        System.out.println("0].EXIT");
+                        ch = sc.nextInt();
                         break;
                     case 0:
+                        exit();
                         return;
                     default:
                         System.out.println("Inavlid Choice!!!");
